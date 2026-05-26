9b326de3f4fe0a5d74f49634cc979aa4c3f3df8d
L3and1r <58855651+L3and1r@users.noreply.github.com>
2026-05-26 13:24:00 +0300

    Update binTrees.cpp

--- lab_binTrees/Kiselev/binTrees.cpp
+++ lab_binTrees/Kiselev/binTrees.cpp
@@ -1,4 +1,8 @@
 #include "binTrees.h"
+#include <queue>
+#include <stack>
+#include <vector>
+#include <utility>
 
 // ============= node =============
 template <typename T>
@@ -15,14 +19,36 @@ binTree<T>::binTree() {
 }
 
 template <typename T>
-node<T>* copyTree(node<T>* current) {
+node<T>* binTree<T>::copyTree(node<T>* current) {
     if (current == nullptr) {
         return nullptr;
     }
-    node<T>* newNode = new node<T>(current->value);
-    newNode->left = copyTree(current->left);
-    newNode->right = copyTree(current->right);
-    return newNode;
+
+    std::stack<std::pair<node<T>*, node<T>**> > stack;
+    node<T>* newRoot = new node<T>(current->value);
+    stack.push(std::make_pair(current, &newRoot));
+
+    while (!stack.empty()) {
+        std::pair<node<T>*, node<T>**> top = stack.top();
+        stack.pop();
+
+        node<T>* origNode = top.first;
+        node<T>* newNode = *(top.second);
+
+        if (origNode->left != nullptr) {
+            node<T>* newLeft = new node<T>(origNode->left->value);
+            newNode->left = newLeft;
+            stack.push(std::make_pair(origNode->left, &newNode->left));
+        }
+
+        if (origNode->right != nullptr) {
+            node<T>* newRight = new node<T>(origNode->right->value);
+            newNode->right = newRight;
+            stack.push(std::make_pair(origNode->right, &newNode->right));
+        }
+    }
+
+    return newRoot;
 }
 
 template <typename T>
@@ -32,11 +58,24 @@ binTree<T>::binTree(const binTree<T>& other) {
 }
 
 template <typename T>
-void clearTree(node<T>* current) {
-    if (current != nullptr) {
-        clearTree(current->left);
-        clearTree(current->right);
-        delete current;
+void binTree<T>::clearTree(node<T>* current) {
+    if (current == nullptr) return;
+
+    std::stack<node<T>*> stack;
+    stack.push(current);
+
+    while (!stack.empty()) {
+        node<T>* node = stack.top();
+        stack.pop();
+
+        if (node->left != nullptr) {
+            stack.push(node->left);
+        }
+        if (node->right != nullptr) {
+            stack.push(node->right);
+        }
+
+        delete node;
     }
 }
 
@@ -46,17 +85,19 @@ binTree<T>::~binTree() {
 }
 
 template <typename T>
-node<T>* insertNode(node<T>* current, const T& val, bool& inserted) {
+node<T>* binTree<T>::insertNode(node<T>* current, const T& val, bool& inserted) {
     if (current == nullptr) {
         inserted = true;
         return new node<T>(val);
     }
-    
+
     if (val < current->value) {
         current->left = insertNode(current->left, val, inserted);
-    } else if (val > current->value) {
+    }
+    else if (val > current->value) {
         current->right = insertNode(current->right, val, inserted);
-    } else {
+    }
+    else {
         inserted = false;
     }
     return current;
@@ -72,15 +113,15 @@ void binTree<T>::insert(const T& val) {
 }
 
 template <typename T>
-node<T>* findMin(node<T>* current) {
-    while (current && current->left != nullptr) {
+node<T>* binTree<T>::findMin(node<T>* current) {
+    while (current != nullptr && current->left != nullptr) {
         current = current->left;
     }
     return current;
 }
 
 template <typename T>
-node<T>* removeNode(node<T>* current, const T& val, bool& removed) {
+node<T>* binTree<T>::removeNode(node<T>* current, const T& val, bool& removed) {
     if (current == nullptr) {
         removed = false;
         return nullptr;
@@ -88,17 +129,19 @@ node<T>* removeNode(node<T>* current, const T& val, bool& removed) {
 
     if (val < current->value) {
         current->left = removeNode(current->left, val, removed);
-    } else if (val > current->value) {
+    }
+    else if (val > current->value) {
         current->right = removeNode(current->right, val, removed);
-    } else {
-
+    }
+    else {
         removed = true;
 
         if (current->left == nullptr) {
             node<T>* temp = current->right;
             delete current;
             return temp;
-        } else if (current->right == nullptr) {
+        }
+        else if (current->right == nullptr) {
             node<T>* temp = current->left;
             delete current;
             return temp;
@@ -129,11 +172,45 @@ bool binTree<T>::find(const T& val) {
     while (current != nullptr) {
         if (val == current->value) {
             return true;
-        } else if (val < current->value) {
+        }
+        else if (val < current->value) {
             current = current->left;
-        } else {
+        }
+        else {
             current = current->right;
         }
     }
     return false;
 }
+
+template <typename T>
+void binTree<T>::printTree() {
+    if (root == nullptr) {
+        std::cout << "Дерево пусто" << std::endl;
+        return;
+    }
+
+    std::cout << "Бинарное дерево (уровневый обход):" << std::endl;
+    std::queue<node<T>*> q;
+    q.push(root);
+
+    while (!q.empty()) {
+        int levelSize = (int)q.size();
+        for (int i = 0; i < levelSize; i++) {
+            node<T>* current = q.front();
+            q.pop();
+
+            if (current != nullptr) {
+                std::cout << current->value << " ";
+                q.push(current->left);
+                q.push(current->right);
+            }
+            else {
+                std::cout << "null ";
+            }
+        }
+        std::cout << std::endl;
+    }
+}
+
+template class binTree<int>;
